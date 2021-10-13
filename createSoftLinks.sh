5d3b6b7a205d2209a5b48b92ccaddaf213c79dbb
David Henry <mgadriver@gmail.com>
2021-10-13 14:53:59 +0300

    Ignore symlinks

---
+++ createSoftLinks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+dirs='examples/Decode examples/Encode examples/Incoming'
+current=$PWD
+for d in $dirs
+do
+	#echo $d
+	#echo $current
+	cd $current
+	cd $d
+	cd lib
+	rm -r pdulib
+	mkdir pdulib
+	cd pdulib
+	ln -s ../../../src/pdulib.cpp pdulib.cpp
+	ln -s ../../../src/pdulib.h pdulib.h
+done
+echo 'All done'
+
